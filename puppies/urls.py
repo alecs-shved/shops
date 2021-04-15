f4e27eae2efd808108d83c2f3a4127c3f3e13078
alecs-shved <shveduk-al@mail.ru>
2021-04-15 14:06:17 +0300

    test2

--- puppies/urls.py
+++ puppies/urls.py
@@ -3,13 +3,14 @@ from . import views
 
 urlpatterns = [
     path(
-        'api/v1/puppies/(?P<pk>[0-9]+)',
-        views.get_delete_update_city,
-        name='get_delete_update_city'
-    ),
-    path(
         'api/v1/puppies/',
         views.get_post_citys,
         name='get_post_city'
-    )
+    ),
+    path(
+        'api/v1/puppies/(?<pk>[0-9]+)',
+        #'api/v1/puppies/(?P<pk>)',
+        views.get_delete_update_city,
+        name='get_delete_update_city'
+    ),
 ]
